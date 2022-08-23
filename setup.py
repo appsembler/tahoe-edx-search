3ce5a072b790c4226436040a727decaa76fdbf59
Omar Al-Ithawi <i@omardo.com>
2022-08-23 09:13:48 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Set up for the edX Search CAG Support package."""
+
+from setuptools import setup
+
+description = 'A hack package to add Course Access Groups support for edx-search.'
+
+setup(
+    name='tahoe-search-cag-support',
+    version='0.1.0',
+    description=description,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    packages=[
+        'tahoe_search_cag_support',
+    ],
+)
